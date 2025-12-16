807970f6347ff7b6adda256fe0e9da108356657e
Sergio Trujillo <sergiotrujillo.ct@gmail.com>
2025-12-16 01:22:39 +0100

    First commit & Exercise 1

---
+++ Dia 1/README.md
@@ -0,0 +1,3 @@
+# Advent of code exercises
+
+The solutions to the exercises from [AdventOfCode](https://adventofcode.com/) will be published here.
---
+++ Dia 1/adviento.py
@@ -0,0 +1,46 @@
+def parse_code(codigo: str) -> int:
+    """
+    Convierte el código en un número negativo o positivo en función de R(+) O L(-)
+    """
+    signo = codigo[0]
+    numero = int(codigo[1:])
+    if (signo == "L"):
+        return -numero
+    else:
+        return numero
+    
+lista = ["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"]
+
+def count_cero(l: list) -> int:
+    numero = 50
+    contador = 0
+    for num in l:
+        numero += parse_code(num)
+        if numero == 0:
+            contador += 1
+        if numero == 100:
+            contador += 1
+            numero = 0
+        if numero > 100:
+            numero -= 100
+        if numero < 0:
+            numero += 100
+    return contador
+
+def read_txt(txt_path: str) -> list:
+    with open(txt_path) as txt:
+        txt_data = txt.read()
+    lista = txt_data.splitlines()
+    return lista
+
+def decorador(func):
+    def wrapper():
+        print("The password is: ")
+        print(func())
+    return wrapper
+
+@decorador
+def adv1():
+    return count_cero(read_txt("adv1.txt"))
+
+adv1()
